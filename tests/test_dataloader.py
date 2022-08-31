5df2b92750e9053619e326b2f2f7d64679160cf2
Pragyanstha <pragyanstha@gmail.com>
2022-08-31 16:12:11 +0000

    test config added

--- tests/test_dataloader.py
+++ tests/test_dataloader.py
@@ -3,8 +3,10 @@ import numpy as np
 from data import Dataset
 
 def test_dataloader():
-    DATA_DIR = "data/Problem_01"
-    dataset = Dataset(DATA_DIR)
+    DATA_DIR = "/workspace/SummerCamp2022/data/Problem_01"
+    RESULT_DIR = "/workspace/SummerCamp2022/results"
+
+    dataset = Dataset(DATA_DIR, RESULT_DIR, fps=5)
 
     img = dataset.get_images(0)
     assert len(img.shape) == 3
