662b6294e3faa1c50afdd95640b3b41f4e75233e
Pragyanstha <pragyanstha@gmail.com>
2022-09-16 00:30:24 +0000

    test predict added

--- tests/test_predict.py
+++ tests/test_predict.py
@@ -18,13 +18,14 @@ from mmdet.utils import (collect_env, get_device, get_root_logger,
                          update_data_root)
 from mmdet.apis import init_detector, inference_detector
 
-checkpoint_file = "work_dirs/faster_rcnn_r50_fpn/latest.pth"
-config_file = "configs/faster_rcnn_r50_fpn.py"
-model = init_detector(config_file, checkpoint_file, device='cuda:0')
 
-# test a single image and show the results
-img = 'data/Problem_04/images/0000.png'  # or img = mmcv.imread(img), which will only load it once
-result = inference_detector(model, img)
+def test_predict():
+    checkpoint_file = "/workspace/SummerCamp2022/work_dirs/faster_rcnn_r50_fpn/latest.pth"
+    config_file = "/workspace/SummerCamp2022/configs/faster_rcnn_r50_fpn.py"
+    model = init_detector(config_file, checkpoint_file, device='cuda:0')
 
-# or save the visualization results to image files
-model.show_result(img, result, out_file='results/test.jpg')
+    # test a single image and show the results
+    img = 'data/Problem_04/images/0120.png'  # or img = mmcv.imread(img), which will only load it once
+    result = inference_detector(model, img)
+    print(result)
+    assert len(result) > 0
